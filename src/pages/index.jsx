d7271c91204f04ef31f1dbdfd1e72a22e2b014d0
hirag <shuto.jp@outlook.com>
2023-11-03 21:48:37 +0900

    ✨ useStateで文字列と真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,9 @@ import { Main } from 'src/components/Main'
 
 export default function Home() {
   const [foo, setFoo] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
 
   const handleClck = useCallback(() => {
     //前の状態を用いて数字を変化させたい場合は、setFooの中に関数を書く
@@ -14,6 +17,10 @@ export default function Home() {
     }
   }, [foo]);
 
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -21,10 +28,17 @@ export default function Home() {
 
     }
   }, [foo])
+
+  console.log(text);
   return (
     <>
-      <h1>{foo}</h1>
+      {isShow ? <h1>{foo}</h1> : null}
+      <input type="text" value={text} onChange={e => {
+        if (text.length >= 5) { return; }
+        setText(e.target.value);
+      }} />
       <button href='/about' onClick={handleClck}>ボタン</button >
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button >
       <Main page="index" />
     </>
   )
